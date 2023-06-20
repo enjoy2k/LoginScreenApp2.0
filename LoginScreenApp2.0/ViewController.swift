c3434a5a39a5d64b10ddd6e28fd499ff1a9d2089
Daniil Kozlov <enjoy2k@mail.ru>
2023-06-20 11:57:52 +0300

    Initial Commit

---
+++ LoginScreenApp2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginScreenApp2.0
+//
+//  Created by Евгения Максимова on 20.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
